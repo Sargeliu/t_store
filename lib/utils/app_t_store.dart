032321298d22dbe7c0b78e4f49de9be299632565
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2024-09-28 23:28:22 +0500

    Design Onboarding Screen Pages

--- lib/utils/app_t_store.dart
+++ lib/utils/app_t_store.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
 class TApp extends StatelessWidget {
@@ -11,6 +12,7 @@ class TApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
--- lib/utils/constants/image_string.dart
+++ lib/utils/constants/image_string.dart
@@ -9,4 +9,12 @@ class TImages {
   // static const String google = 'assets/logos/google-icon.png'
   static const String google = '';
   static const String facebook = '';
+
+  // -- OnBoarding Images
+  static const String onBoardingImage1 = 'assets/images/on_boarding_images/sa'
+      'mmy-line-sailor-on-mast-looking-through-telescope.gif';
+  static const String onBoardingImage2 = 'assets/images/on_boarding_images/sa'
+      'mmy-line-woman-in-shopping-cart-placing-order-on-smartphone.gif';
+  static const String onBoardingImage3 = 'assets/images/on_boarding_images/sa'
+      'mmy-line-man-and-dog-delivering-packages-on-a-moped.gif';
 }
