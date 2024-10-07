4754483c7ad09473c4fcd9b126841926be3663ac
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2024-10-07 22:44:25 +0500

    Custom shape clip path.

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:t_store/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,6 +12,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(child: Container(),),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+
+
